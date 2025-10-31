b45f2f0f69b5e2fa6783d043d53d6433a1cba352
Cyphonvoid <yashaswi.kul@gmail.com>
2025-10-30 17:22:44 -0700

    TATAT

--- app.py
+++ app.py
@@ -2,10 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.route("/")
+@app.route("/")
 def home():
-    return "hello"
+    return "Hello, Vercel!"
 
+@app.route("/api")
+def api():
+    return "Hello, API!"
+
+@app.route("/turtle")
+def api():
+    return "Hello, Turtle!"
 
 if __name__ == "__main__":
     app.run()
